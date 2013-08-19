67bf45a35bdbd7119fa45c64a2607ac89c2e2288
Chaitanya <cpalaka@gmail.com>
2013-08-20 02:59:28 +0530

    minor edit

---
+++ dynamicEntity.h
@@ -0,0 +1,13 @@
+#ifndef DYNAMICENTITY_H
+#define DYAMICENTITY_H
+
+#include "Drawable.h"
+
+class dynamicEntity : public Drawable {
+
+public:
+
+private:
+};
+
+#endif DYNAMICENTITY_H;
--- staticEntity.h
+++ staticEntity.h
@@ -8,7 +8,6 @@ public:
 	staticEntity(std::string);
 protected:
 	b2Body* body;
-	int a;
 };
 
 #endif STATICENTITY_H
